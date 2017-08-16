5f066eff9c115660f48c54d91b10b7dbb3d7853b
MartinLaursen <111317@rts-365.dk>
2017-08-16 12:57:34 +0200

    test

---
+++ Variabler/variable1.js
@@ -0,0 +1,5 @@
+//variabler
+var navn="martin";
+var alder="23";
+var tekst="Mit navn er "+ navn +" og jeg er "+ alder +" år gammel";
+console.log(tekst);
---
+++ Variabler/variable2.js
@@ -0,0 +1,6 @@
+var moms="1.25";
+var pris_uden_moms="400";
+var pris_med_moms= pris_uden_moms * moms;
+var pris_uden_moms2 = pris_med_moms / moms;
+console.log(pris_med_moms);
+console.log(pris_uden_moms2);
